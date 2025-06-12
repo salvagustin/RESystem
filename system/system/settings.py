a3dad890362b39a68fe14f67dbf18c783fa6f390
salvagustin <agusbmx97@gmail.com>
2025-06-12 15:21:00 -0600

    con host

--- system/system/settings.py
+++ system/system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tso!0&8fln0l^^_0o#!w=+9rq@rw8eu=y35j=fxc3hvr9dghw3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','34.55.130.214 ']
 LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
 
