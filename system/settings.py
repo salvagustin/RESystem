f40362aed2c857fa2117410b49909586c44a086f
salvagustin <agusbmx97@gmail.com>
2025-08-13 21:43:30 -0600

    correccion form venta y corte

--- system/settings.py
+++ system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tso!0&8fln0l^^_0o#!w=+9rq@rw8eu=y35j=fxc3hvr9dghw3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = ['localhost','34.95,40.193']
+ALLOWED_HOST = ['localhost','34.95.40.193']
 
 LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
